dd300054d1b4481296460f4b71ac272d3bd60c36
Shayne Rushton <mshaynerush@gmail.com>
2023-11-20 08:36:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const hbs = require('hbs');
-require('./app_server/models/db');
+require('./app_server/database/db');
 
 const indexRouter = require('./app_server/routes/index');
 const aboutRouter = require('./app_server/routes/about');
